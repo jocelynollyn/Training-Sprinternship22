28ae36e3f41aaf8202bc9b8b2a131fc9e4351df3
jocelynollyn <ollynzz@gmail.com>
2022-05-09 13:58:36 -0500

    Update programatic_api_calls.py

--- exercises/programatic_api_calls.py
+++ exercises/programatic_api_calls.py
@@ -3,14 +3,18 @@ import requests
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
 # make get request
+response = requests.get(url)
 
 
-# check if respons status code is 200
-
+# check if response status code is 200
+if response.status_code == 200:
     # get response body in text
-
+    print(response.text)
 
     # convert response body to JSON
+    data = response.json()
 
 
 # otherwise, print error code
+else:
+    print(response.status_code)
