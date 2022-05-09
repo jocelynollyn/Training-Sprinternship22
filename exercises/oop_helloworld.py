153a111f27b5fcb67f38383f2e0f5e584c377a50
jocelynollyn <ollynzz@gmail.com>
2022-05-09 13:10:56 -0500

    Update oop_helloworld.py

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,17 @@
 # create your class here
+class Car:
+    def __init__(self, Model, Speed):
+        self.model = Model
+        self.speed = Speed
+        
+
+    def getModel(self):
+        return self.model
+
+    def setSpeed(self, newSpeed):
+        self.speed = newSpeed
+
+
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
