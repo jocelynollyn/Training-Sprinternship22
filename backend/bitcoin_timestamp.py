d356cec10dfd87afa28046dc4fe9677241f63134
jocelynollyn <ollynzz@gmail.com>
2022-05-10 11:14:38 -0500

    Update bitcoin_timestamp.py

--- backend/bitcoin_timestamp.py
+++ backend/bitcoin_timestamp.py
@@ -1,4 +1,7 @@
 class BitcoinTimestamp:
     # TODO: (5.2)
     def __init__(self, timestamp: str, price: float):
+        self.timestamp = timestamp
+        self.price = price
+        
         pass
