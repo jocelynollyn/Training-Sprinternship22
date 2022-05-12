607955cc6033b3a330a379a49b517cc620256690
jocelynollyn <ollynzz@gmail.com>
2022-05-12 10:44:09 -0500

    Update program.js

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -1,10 +1,16 @@
 var body;
 
 // implement onload function
-
+window.onload = function() {
+    body = document.querySelector("body");
+}
 
 // implement changeColorBlue
-
+window.changeColorBlue = function() {
+    body.style.backgroundColor = "#a8ceff";
+}
 
 // implement changeColorPurple
-
+window.changeColorGreen = function() {
+    body.style.backgroundColor = "#148F77";
+}
